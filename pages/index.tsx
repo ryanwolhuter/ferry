042b4e32d24c832fbb1e99443d9fcf9bb55acc0e
ryanwolhuter <ryanw@linumlabs.com>
2021-08-04 17:35:52 +0200

    Extract upload form to component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { useState, ChangeEvent, MouseEvent } from 'react'
-import { Web3Storage } from 'web3.storage'
-import axios from 'axios'
-
-function makeStorageClient() {
-  const token = process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN ?? ''
-  return new Web3Storage({ token })
-}
+import UploadForm from '../components/UploadForm'
 
 export default function Home() {
-  const [isUploadButtonDisabled, setIsUploadButtonDisabled] = useState(true)
-  const [files, setFiles] = useState<File[]>([])
-  const [fileName, setFileName] = useState('')
-  const [fileUrl, setFileUrl] = useState('')
-  const [rootCid, setRootCid] = useState('')
-  const [percentUploaded, setPercentUploaded] = useState('0%')
-  const [email, setEmail] = useState('')
-
-  async function storeWithProgress(files: File[]) {
-
-    const onRootCidReady = (rootCid: string) => {
-      setRootCid(rootCid)
-    }
-  
-    const totalSize = 
-      files
-      .map(f => f.size)
-      .reduce((a, b) => a + b)
-
-    let uploaded = 0
-  
-    const onStoredChunk = (size: number) => {
-      uploaded += size
-      const percentUploaded = `${Math.round(uploaded / totalSize * 100)}%`
-      setPercentUploaded(percentUploaded)
-    }
-  
-    const client = makeStorageClient()
-
-    return client.put(files, { onRootCidReady, onStoredChunk })
-  }
-  
-
-  function handleInput(inputEvent: ChangeEvent<HTMLInputElement>) {
-    const files = Array.from(inputEvent?.target?.files ?? [])
-    setFiles(files)
-    setFileName(files?.[0]?.name)
-    setFileUrl(`https://${rootCid}.ipfs.dweb.link/${fileName}`)
-    setIsUploadButtonDisabled(false)
-  }
-
-  async function handleSubmit(submitEvent: MouseEvent) {
-    submitEvent.preventDefault()
-
-    await storeWithProgress(files)
-
-    axios.post('/api/send-mail', {
-      email,
-      url: fileUrl
-    })
-  }
-
   return (
     <div className={styles.container}>
       <Head>
@@ -75,12 +16,7 @@ export default function Home() {
           Ferry
         </h1>
 
-      <input type="file" name="fileInput" onChange={e => handleInput(e)}></input>
-      <input type="text" name="email" onChange={e => setEmail(e.target.value)}></input>
-      <button onClick={e => handleSubmit(e)} disabled={isUploadButtonDisabled}>Submit</button>
-      <div>Percent uploaded: {percentUploaded}</div>
-      <div>Result: https://dweb.link/ipfs/{rootCid}</div>
-      <div>Direct <a download={fileName} href={`https://${rootCid}.ipfs.dweb.link/${fileName}`}>download</a></div>
+        <UploadForm />
       </main>
 
       <footer className={styles.footer}>

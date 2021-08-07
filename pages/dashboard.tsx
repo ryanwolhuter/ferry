acd99851d5508389dbdc06aa566a85faf8340b2f
ryanwolhuter <ryanw@linumlabs.com>
2021-08-07 12:06:15 +0200

    Add upload form to dashboard.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,14 @@
 import useAuth from "../hooks/useAuth"
+import UploadForm from '../components/UploadForm'
 
 export default function Dashboard() {
   const { user, loading } = useAuth()
   
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1>Ferry</h1>
       {loading ? 'Loading...' : user.email}
+      <UploadForm />
     </>
   )
 }

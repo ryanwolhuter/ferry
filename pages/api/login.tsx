7f05b6b469bafbf55974a6bd6c8c97a67cc4383f
ryanwolhuter <ryanw@linumlabs.com>
2021-08-07 11:24:23 +0200

    perform login with cookie service

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { Magic } from '@magic-sdk/admin'
 import Iron from '@hapi/iron'
-import CookieService from '../../lib/cookie.ts'
+import CookieService from '../../lib/cookie'
 
 let magic = new Magic(process.env.MAGIC_SECRET_KEY)
 
@@ -9,9 +9,12 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end()
 
   // exchange the DID from Magic for some user data
-  const did = magic.utils.parseAuthorizationHeader(req.headers.authorization)
-  const user = await magic.users.getMetadataByToken(did)
+  const did = req.headers.authorization.split('Bearer').pop().trim()
+  const user = await new Magic(process.env.MAGIC_SECRET_KEY).users.getMetadataByToken(did)
 
-  // TODO Author a couple of cookies to persist a user's session
+
+  // Author a couple of cookies to persist a user's session
+  const token = await Iron.seal(user, process.env.ENCRYPTION_SECRET, Iron.defaults)
+  CookieService.setTokenCookie(res, token)
   res.end()
 }

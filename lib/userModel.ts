e32bec685886fc4e4d59217567433118df1ed9ba
ryanwolhuter <ryanw@linumlabs.com>
2021-08-08 12:27:01 +0200

    Add user model.

---
+++ lib/userModel.ts
@@ -0,0 +1,19 @@
+import { q, adminClient, getClient } from './faunadb'
+
+export class UserModel {
+  async createUser(email) {
+
+  }
+
+  async getUserByEmail(email) {
+
+  }
+
+  async obtainFaunaDBToken(user) {
+
+  }
+
+  async invalidateFaunaDBToken(token) {
+    
+  }
+}

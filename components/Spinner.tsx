bc24c61bd6a9ba1d9bb5760a11abe8063b27b5d8
ryanwolhuter <ryanw@linumlabs.com>
2021-08-08 16:15:22 +0200

    Fix incorrect style import

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,4 +1,4 @@
-import styles from 'styles/Spinner.module.css'
+import styles from '../styles/Spinner.module.css'
 
 export default function Spinner() {
   return <div className={styles.spinner} aria-label="loading"></div>

1b4afd81a0b70f21547cf761de2d9023d2cb8ebf
ryanwolhuter <ryanw@linumlabs.com>
2021-08-08 15:06:06 +0200

    Add layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import { useUser } from '../lib/hooks'
+
+export default function Layout({ children }) {
+  const { user } = useUser()
+
+  return (
+    <>
+    <Head>
+      <title>Ferry</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <header>
+      <h1>Your Files</h1>
+    </header>
+
+    <main>
+      <div className="container">{children}</div>
+      {user &&
+        <div>
+          <span>{user.email}</span>
+          <a href="/api/logout">Logout</a>
+        </div>}
+    </main>
+    </>
+  )
+}

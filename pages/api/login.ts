07c6c17abb28de04c0d1091edf707ad761cd7f48
ryanwolhuter <ryanw@linumlabs.com>
2021-08-08 14:16:50 +0200

    Update login to use new services

---
+++ pages/api/login.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { Magic } from '@magic-sdk/admin'
+import { createSession } from "../../lib/cookie"
+import { createHandlers } from "../../lib/restUtils"
+import { UserModel } from "../../lib/userModel"
+
+const handlers = {
+  POST: async (req: NextApiRequest, res: NextApiResponse) => {
+    // validate the users did token
+    const magic = new Magic(process.env.MAGIC_SECRET_KEY)
+    const didToken = magic.utils.parseAuthorizationHeader(req.headers.authorization)
+    const { email, issuer } = await magic.users.getMetadataByToken(didToken)
+
+    // get or create a user's entity in faunadb
+    const userModel = new UserModel()
+
+    // auto-detect signups if `getUserByEmail` is null
+    const user = await userModel.getUserByEmail(email) ?? await userModel.createUser(email)
+    const token = await userModel.obtainFaunaDBToken(user)
+
+    // create session cookie with the user's verified information
+    await createSession(res, { token, email, issuer })
+
+    res.status(200).send({ done: true })
+  }
+}
+
+export default function login(req: NextApiRequest, res: NextApiResponse) {
+  const handler = createHandlers(handlers)
+  return handler(req, res)
+}

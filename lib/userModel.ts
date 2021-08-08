0038da5cbb7a4961e57d4cdb5d80655088e73cb4
ryanwolhuter <ryanw@linumlabs.com>
2021-08-08 13:24:07 +0200

    Add get user by email

--- lib/userModel.ts
+++ lib/userModel.ts
@@ -2,13 +2,21 @@ import { q, adminClient, getClient } from './faunadb'
 
 export class UserModel {
   async createUser(email: string) {
-    return adminClient.query(q.Create(q.Collection('users'), {
-      data: { email }
-    }))
+    return adminClient
+      .query(
+        q.Create(
+          q.Collection('users'), {
+          data: { email }
+        }))
   }
 
-  async getUserByEmail(email) {
-
+  async getUserByEmail(email: string) {
+    return adminClient
+      .query(
+        q.Get(
+          q.Match(
+            q.Index('users_by_email'), email))
+      ).catch(() => null)
   }
 
   async obtainFaunaDBToken(user) {

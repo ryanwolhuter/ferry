40ae9503d8406ce0ac4979b0e21179634f0cb1ee
ryanwolhuter <ryanw@linumlabs.com>
2021-08-09 20:16:27 +0200

    Add fallback auth header

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -8,14 +8,15 @@ const handlers = {
   POST: async (req: NextApiRequest, res: NextApiResponse) => {
     // validate the users did token
     const magic = new Magic(process.env.MAGIC_SECRET_KEY)
-    const didToken = magic.utils.parseAuthorizationHeader(req.headers.authorization)
+    const didToken = magic.utils.parseAuthorizationHeader(req.headers.authorization ?? 'NO AUTH HEADER')
     const { email, issuer } = await magic.users.getMetadataByToken(didToken)
 
     // get or create a user's entity in faunadb
     const userModel = new UserModel()
 
     // auto-detect signups if `getUserByEmail` is null
-    const user = await userModel.getUserByEmail(email) ?? await userModel.createUser(email)
+    const user = await userModel.getUserByEmail(email)
+      ?? await userModel.createUser(email)
     const token = await userModel.obtainFaunaDBToken(user)
 
     // create session cookie with the user's verified information

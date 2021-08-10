eff0603fec99bfaed6e6eff07dab0c1835802e65
ryanwolhuter <ryanw@linumlabs.com>
2021-08-10 11:31:05 +0200

    Change to named exports

--- lib/fileUpload.ts
+++ lib/fileUpload.ts
@@ -1,8 +1,8 @@
-function getFileName(files: File[]) {
+export function getFileName(files: File[]) {
   if (!files) return ''
   return encodeURI(files?.[0]?.name)
 }
 
-export default function makeFileUrl(cid: string, files: File[]) {
+export function makeFileUrl(cid: string, files: File[]) {
   return `https://${cid}.ipfs.dweb.link/${getFileName(files)}`
 }

023d25c6bb8637cda6ed49997351c92bb9ccd43f
ryanwolhuter <ryanw@linumlabs.com>
2021-08-10 15:51:49 +0200

    Add spinner for file upload

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -3,15 +3,19 @@ import { useState, ChangeEvent, FormEvent } from 'react'
 import makeStorageClient from '../lib/storageClient'
 import { getFileName, makeFileUrl } from '../lib/fileUpload'
 import sendEmail from '../lib/sendEmail'
+import Spinner from './Spinner'
 
 export default function UploadForm({ mutateUploads }) {
   const [files, setFiles] = useState<File[]>([])
   const [rootCid, setRootCid] = useState('')
   const [percentUploaded, setPercentUploaded] = useState('0%')
   const [email, setEmail] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   async function storeWithProgress(files: File[]) {
     try {
+      setIsLoading(true)
+
       const client = makeStorageClient()
 
       const totalSize = files
@@ -30,9 +34,12 @@ export default function UploadForm({ mutateUploads }) {
         setRootCid(rootCid)
       }
     
-      return client.put(files, { onRootCidReady, onStoredChunk })
+      const cid = await client.put(files, { onRootCidReady, onStoredChunk })
+      return cid
     } catch (error) {
       console.error(error)
+    } finally {
+      setIsLoading(false)
     }
   }
   
@@ -62,34 +69,39 @@ export default function UploadForm({ mutateUploads }) {
   }
 
   return (
-  <form onSubmit={e => handleSubmit(e)}>
-      <label htmlFor="fileInput">
-        Choose file
-        <input 
-          type="file" 
-          name="fileInput" 
-          onChange={e => handleChooseFile(e)}
-        ></input>
-      </label>
-      <label htmlFor="email">
-        Receiver email
-        <input 
-          type="text" 
-          name="email" 
-          onChange={e => setEmail(e.target.value)}
-        ></input>
-      </label>
-      <button 
-        type="submit" 
-        disabled={files.length === 0}
-      >Submit
-      </button>
-      { rootCid ? (
-        <>
-          <div>Percent uploaded: {percentUploaded}</div>
-          <div>Direct <a href={makeFileUrl(rootCid, files)}>download</a></div>
-        </>
-      ) : <div></div>}
-  </form>
-  )
+    <>
+      {isLoading 
+        ? <Spinner />
+        : <form onSubmit={e => handleSubmit(e)}>
+            <label htmlFor="fileInput">
+              Choose file
+              <input 
+                type="file" 
+                name="fileInput" 
+                onChange={e => handleChooseFile(e)}
+              ></input>
+            </label>
+            <label htmlFor="email">
+              Receiver email
+              <input 
+                type="text" 
+                name="email" 
+                onChange={e => setEmail(e.target.value)}
+              ></input>
+            </label>
+            <button 
+              type="submit" 
+              disabled={files.length === 0}
+            >Submit
+            </button>
+            { rootCid ? (
+              <>
+                {/* <div>Percent uploaded: {percentUploaded}</div> */}
+                <div>Direct <a href={makeFileUrl(rootCid, files)}>download</a></div>
+              </>
+            ) : <div></div>}
+        </form>  
+      }
+    </>
+)
 }

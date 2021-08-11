09b3ee12b25124bc6793954918f29048540999e6
ryanwolhuter <ryanw@linumlabs.com>
2021-08-11 10:22:27 +0200

    Add background image component to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Background from './Background'
 import { useUser } from '../lib/hooks'
+import styles from '../styles/Layout.module.css'
 
 type LayoutProps = {
   children: JSX.Element
@@ -11,25 +13,22 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <>
-    <Head>
-      <title>Ferry</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+      <Head>
+        <title>Ferry</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Background />
 
-    <header>
-      <h1>Ferry</h1>
-    </header>
-
-    <main>
-      <div className="container">{children}</div>
-      {user &&
-        <div>
-          <span>{user.email}</span>
-          <Link href="/api/logout">
-            <a>Logout</a>
-          </Link>
-        </div>}
-    </main>
+      <main className={styles.main}>
+        <div className={styles.container}>{children}</div>
+        {user &&
+          <div>
+            <span>{user.email}</span>
+            <Link href="/api/logout">
+              <a>Logout</a>
+            </Link>
+          </div>}
+      </main>
     </>
   )
 }

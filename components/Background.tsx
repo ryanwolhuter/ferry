2710fb64cbaa1185772c1a2c7542246c3f4c62fb
ryanwolhuter <ryanw@linumlabs.com>
2021-08-12 19:18:54 +0200

    update background

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
-import background from '../public/background.png'
+import background from '../public/background.jpg'
 import styles from '../styles/Background.module.css'
 
 export default function Background() {
   return (
     <div className={styles.bgWrap}>
       <Image
+        priority
         alt="background image"
         src={background}
         layout="fill"

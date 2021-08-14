6cda44d7dd03917ec07a4c55a77ef531d0a7edb0
ryanwolhuter <ryanw@linumlabs.com>
2021-08-14 17:39:28 +0200

    Add background image

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import background from '../public/background.jpg'
+import background from '../public/background.png'
 import styles from '../styles/Background.module.css'
 
 export default function Background() {

9c518493773ecddf3ff57a7258e60e92619cec23
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-08-14 14:09:09 +0200

    Added contract addresses

--- constants/chain.ts
+++ constants/chain.ts
@@ -1,7 +1,7 @@
 export const chainID = 80001
 export const contracts = {
-    ferry: "",
-    minter: "",
-    shipToken: "",
-    shipStaking: ""
+    shipToken: "0x1EFcb17a4F5B628E82Fe8048D3B41ED0fdF375E3",
+    shipStaking: "0x74972961bbC7739403cA9ceec7Bab1290E1b555d",
+    ferry: "0x0D3e25E6B337bdf1F6293609F59133C57522c0A8",
+    minter: "0x26205b8667286bB528f9D81af40A9415Ec30a3fa"
 } 

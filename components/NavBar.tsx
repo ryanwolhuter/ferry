c5ec1f77ca47a67936eef9f10b113cceccb86270
ryanwolhuter <ryanw@linumlabs.com>
2021-08-15 16:45:07 +0200

    Add navbar styles

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,52 +2,81 @@ import styles from '../styles/NavBar.module.css'
 import { useUser } from '../lib/hooks'
 import Link from 'next/link'
 import Button from './Button'
+import Image from 'next/image'
+import logo from '../public/logo.svg'
 
-var Contract = require('web3-eth-contract');
-import Web3Modal from "web3modal";
+// var Contract = require('web3-eth-contract');
+// import Web3Modal from "web3modal";
 
-import { contractAddresses, abis } from '../constants/chain'
+// import { contractAddresses, abis } from '../constants/chain'
 
 export default function NavBar(props: any) {
   const { user } = useUser()
 
-  const { provider, updateProvider } = props
+  // const { provider, updateProvider } = props
 
-  const buttonLabel = provider ? "Disconnect Wallet" : "Connect Wallet"
-  const userAddress = provider ? provider.selectedAddress : "No wallet found"
+  // const buttonLabel = provider ? "Disconnect Wallet" : "Connect Wallet"
+  // const userAddress = provider ? provider.selectedAddress : "No wallet found"
 
-  const handleClick = async () => {
-    if (provider) {
-      // Disconnect Wallet
-      updateProvider(null)
-    } else {
-      // Connect Wallet
-      const providerOptions = {}
-      const web3Modal = new Web3Modal({
-        network: "mumbai", // optional
-        cacheProvider: true, // optional
-        providerOptions // required
-      });
-      const prov = await web3Modal.connect();
-      updateProvider(prov)
-    }
-  }
+  // const handleClick = async () => {
+  //   if (provider) {
+  //     // Disconnect Wallet
+  //     updateProvider(null)
+  //   } else {
+  //     // Connect Wallet
+  //     const providerOptions = {}
+  //     const web3Modal = new Web3Modal({
+  //       network: "mumbai", // optional
+  //       cacheProvider: true, // optional
+  //       providerOptions // required
+  //     });
+  //     const prov = await web3Modal.connect();
+  //     updateProvider(prov)
+  //   }
+  // }
 
-  const test = async () => {
-    console.log(provider);
+  // const test = async () => {
+  //   console.log(provider);
 
-    Contract.setProvider(provider);
+  //   Contract.setProvider(provider);
 
-    const DaiContract = new Contract(abis.ERC20, contractAddresses.dai);
+  //   const DaiContract = new Contract(abis.ERC20, contractAddresses.dai);
 
-    DaiContract.methods.approve(contractAddresses.ferry, 0).send({
-      from: provider.selectedAddress
-    })
-  }
+  //   DaiContract.methods.approve(contractAddresses.ferry, 0).send({
+  //     from: provider.selectedAddress
+  //   })
+  // }
 
   return (
     <nav className={styles.navbar}>
+      <div className={styles.imageWrapper}>
+        <Image
+          src={logo}
+          alt="Ferry logo"
+        />
+      </div>
+      <Link href="/about">
+        <a className={styles.link}>About</a>
+      </Link>
+      <Link href="/">
+        <a className={styles.link}>Pro Account</a>
+      </Link>
       {user &&
+        <button className={styles.userMenu}>
+          <div>
+            <style jsx>{`
+                div {
+                  width: 32px;
+                  height: 32px;
+                  border-radius: 100%;
+                  background: linear-gradient(77.23deg, #F173CD 4.4%, #E394C0 45.66%, #FFB800 111.45%);
+                  margin-right: 8px;
+                }
+              `}</style>
+          </div>
+          {user?.email}
+        </button>}
+      {/* {user &&
         <>
           <div style={{
             display: 'flex',
@@ -63,7 +92,7 @@ export default function NavBar(props: any) {
           <Link href="/api/logout">
             <a>Logout</a>
           </Link>
-        </>}
+        </>} */}
     </nav>
   )
 }

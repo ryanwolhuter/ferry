98b5e3e60492e7529b24a65999af57d7cc7ffc81
ryanwolhuter <ryanw@linumlabs.com>
2021-09-05 09:17:47 +0200

    Remove axios.

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -1,9 +1,13 @@
-import axios from 'axios'
-
 export default async function sendEmail(email: string, url: string, sender: string) {
-  await axios.post('/api/send-mail', {
-    email,
-    url,
-    sender
-  })
+  await fetch(
+    '/api/send-mail', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email, url, sender
+    })
+  }
+)
 }

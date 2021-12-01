4875af8014b4297523fda111bc36e6d2096a4372
ryanwolhuter <ryanw@linumlabs.com>
2021-12-01 16:58:53 +0200

    use styled components for button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,63 @@
-// @ts-ignore
-import classNames from 'classnames'
-import { useState } from 'react'
+import styled from 'styled-components'
 
-export default function Button(props: any) {
-  const { className, ...otherProps } = props
-  const [isSelected, setIsSelected] = useState(false)
+const ButtonBase = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 24px;
+  border-radius: 10px;
+  width: 101px;
+  height: 48px;
+  border: none;
+  outline: none;
+  white-space: nowrap;
+`
 
+const PrimaryButton = styled(ButtonBase)`
+    color: var(--white);
+    background: var(--purple-gradient);
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 24px;
+
+    &:hover {
+      box-shadow: 0px 0px 17px rgba(116, 6, 226, 0.37);
+    }
+
+    &:active {
+      box-shadow: 0px 0px 17px rgba(116, 6, 226, 0.37);
+      border: 1px solid;
+      border-image-source: linear-gradient(
+        77.23deg,
+        #f173cd 4.4%,
+        #e394c0 45.66%,
+        #ffb800 111.45%
+      );
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 24px;
+      letter-spacing: 0em;
+      text-align: center;
+    }
+
+    &:disabled {
+      background: var(--brown);
+      color: var(--grey);
+      box-shadow: none;
+      font-style: normal;
+      font-weight: normal;
+    }
+`
+
+export default function Button({ variant, children, ...props }) {
+  let Button = PrimaryButton
+      
   return (
-    <>
-      <button
-        onClick={e => setIsSelected(true)}
-        className={classNames(className, isSelected && 'selected')}
-        {...otherProps} />
-    </>
+    <Button {...props}>
+      {children}
+    </Button>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import AppContext from '../context/AppContext'
 import { ContractSet, getContracts } from '../lib/contracts/ContractBooter'
+import { createGlobalStyle } from 'styled-components'
 
 export const InitialContracts = {
   ferryContract: null,
@@ -11,6 +12,65 @@ export const InitialContracts = {
   shipStakingContract: null,
   daiContract: null
 }
+
+const GlobalStyles = createGlobalStyle`
+:root {
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: "Poppins", sans-serif;
+  }
+
+
+* {
+  box-sizing: border-box;
+  color: var(--black);
+  transition: all 0.2s;
+}
+
+  --white: hsl(0, 0%, 100%);
+  --black: #404040;
+  --turquoise: #49fdfd;
+  --blue: #5295a5;
+  --purple: #a775f3;
+  --yellow: #f8ce64;
+  --pink: #fa74ae;
+  --brown: #857987;
+  --grey: #b1b1b1;
+  --turquoise-gradient: linear-gradient(
+    130.61deg,
+    #00e0ff 14.74%,
+    #66d8ca 67.28%
+  );
+  --blue-gradient: linear-gradient(
+    121.46deg,
+    #6aaab9 8.44%,
+    #437581 47.02%,
+    #34565f 83.26%
+  );
+  --purple-gradient: linear-gradient(113.28deg, #d982f8 8.12%, #8444e6 92.8%);
+  --yellow-gradient: linear-gradient(148.58deg, #fee146 29.61%, #f8ce64 82.57%);
+  --pink-gradient: linear-gradient(
+    110.69deg,
+    #fe9dc7 7.16%,
+    #fa74ae 48.77%,
+    #be4579 88.66%
+  );
+  --pink-orange-gradient: linear-gradient(
+    77.23deg,
+    #f173cd 4.4%,
+    #e394c0 45.66%,
+    #ffb800 111.45%
+  );
+  --purple-pink-orange-gradient: linear-gradient(
+    77.23deg,
+    #8055fc 26.71%,
+    #e394c0 80.79%,
+    #ffb800 111.45%
+  );
+}
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -32,6 +92,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       contracts,
       setContracts,
     }}>
+      <GlobalStyles />
       <Component {...pageProps} />
     </AppContext.Provider>
   )

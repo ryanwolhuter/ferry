5173db5d525b4850292480375f84edfbb52c3d04
ryanwolhuter <ryanw@linumlabs.com>
2021-12-02 16:42:01 +0200

    convert nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,15 +1,13 @@
 import { useContext } from 'react'
 import Web3Modal from 'web3modal'
-
-import styles from './NavBar.module.scss'
 import { useUser } from '../lib/hooks'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../public/logo.svg'
 import { useRouter } from 'next/router'
-
 import AppContext from '../context/AppContext'
 import { useState } from 'react'
+import styled from 'styled-components'
 
 export default function NavBar(props: any) {
   const { user } = useUser()
@@ -44,40 +42,124 @@ export default function NavBar(props: any) {
     await router.push('/login')
   }
 
+  const Nav = styled.nav`
+    position: fixed;
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 50% 1fr 1fr auto;
+    height: 4.5rem;
+    width: 100vw;
+    align-items: center;
+    z-index: 2;
+    background: rgba(251, 251, 251, 0.05);
+    box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.25),
+      inset 0px 0px 8px rgba(255, 255, 255, 0.29);
+    backdrop-filter: blur(140px);
+  `
+
+  const ImageWrapper = styled.div`
+    margin-left: 1.5rem;
+    margin-top: 0.5rem;
+    cursor: pointer;
+  `
+
+  const A = styled.a`
+    text-decoration: none;
+    text-align: center;
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    cursor: pointer;
+    height: 100%;
+
+    &:hover {
+      background: var(--purple-pink-orange-gradient);
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
+      border-bottom: 2px solid var(--purple);
+    }
+  `
+
+  const MenuButton = styled.button`
+    width: fit-content;
+  font-weight: bold;
+  margin-left: 24px;
+  margin-right: 24px;
+  padding-left: 12px;
+  background: rgba(251, 251, 251, 0.05);
+  box-shadow: 0px 0px 25px rgba(98, 11, 129, 0.12);
+  cursor: pointer;
+  `
+
+  const Options = styled.div`
+  display: grid;
+  padding: 32px;
+  position: absolute;
+  align-content: center;
+  justify-content: center;
+  gap: 1rem;
+  width: 230px;
+  right: 16px;
+  top: 5rem;
+  background: white;
+  background: #ffffff;
+  box-shadow: 0px 0px 25px rgba(98, 11, 129, 0.12);
+  border-radius: 30px;
+  transition: all 0.4s;
+
+  &.hidden {
+    top: -500px;
+    opacity: 0;
+  }
+  `
+
+  const Button = styled.button`
+  width: 140px;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0px 0px 17px rgba(116, 6, 226, 0.37);
+  }
+
+  &:first-child {
+    background: var(--purple-pink-orange-gradient);
+  }
+
+  &:nth-child(2) {
+    background: var(--pink-orange-gradient)
+  }
+
+  &:nth-child(3) {
+    background: var(--white);
+    border: 1px solid var(--pink);
+    color: var(--pink);
+  }
+  `
+
   return (
-    <nav className={styles.navbar}>
-      <div onClick={e => router.push('/')} className={styles.imageWrapper}>
+    <Nav>
+      <ImageWrapper onClick={() => router.push('/')}>
         <Image
           src={logo}
           alt="Ferry logo"
         />
-      </div>
-      <Link href="/about">
-        <a className={styles.link}>About</a>
+      </ImageWrapper>
+      <Link href="/about" passHref>
+        <A>About</A>
       </Link>
-      <a onClick={e => props.toggleShowSubscribeForm()} className={styles.link}>Pro Account</a>
+      <A onClick={() => props.toggleShowSubscribeForm()}>Pro Account</A>
       {user &&
-        <button className={styles.userMenu} onClick={e => setShowUserOptions(!showUserOptions)}>
-          <div>
-            <style jsx>{`
-                div {
-                  width: 32px;
-                  height: 32px;
-                  border-radius: 100%;
-                  background: linear-gradient(77.23deg, #F173CD 4.4%, #E394C0 45.66%, #FFB800 111.45%);
-                  margin-right: 8px;
-                }
-              `}</style>
-          </div>
+        <MenuButton onClick={() => setShowUserOptions(!showUserOptions)}>
           {user?.email}
           <br />
           {userAddress}
-        </button>}
-          <div className={`${styles.userOptions} ${showUserOptions ? '' : styles.hidden}`}>
-              <button onClick={e => handleConnectWallet()} className={styles.button}>Connect Wallet</button>
-              <button onClick={async e => await router.push('/dashboard')} className={styles.button}>Dashboard</button>
-              <button onClick={e => logout()} className={styles.button}>Logout</button>
-          </div>
-    </nav>
+        </MenuButton>}
+      <Options>
+        <Button onClick={() => handleConnectWallet()}>Connect Wallet</Button>
+        <Button onClick={async () => await router.push('/dashboard')}>Dashboard</Button>
+        <Button onClick={() => logout()}>Logout</Button>
+      </Options>
+    </Nav>
   )
 }

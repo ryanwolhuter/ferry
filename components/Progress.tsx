d23a3d3fff9a3bd47f00460646c9975b98c2e1fc
ryanwolhuter <ryanw@linumlabs.com>
2021-12-03 09:42:18 +0200

    convert progress

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -1,25 +1,65 @@
-export default function Progress({ radius = 100, stroke = 10, progress = 0 }) {
+import styled from 'styled-components'
 
+export default function Progress({ radius = 100, stroke = 10, progress = 0 }) {
   const normalizedRadius = radius - stroke * 2
   const circumference = normalizedRadius * 2 * Math.PI
-  const strokeDashoffset = circumference - progress / 100 * circumference
+  const strokeDashoffset = circumference - (progress / 100) * circumference
+
+  const Wrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+  `
+
+  const Progress = styled.div`
+    width: ${radius * 2 - stroke * 3}px;
+    height: ${radius * 2 - stroke * 3}px;
+    position: absolute;
+    top: ${stroke * 1.5}px;
+    left: ${stroke * 1.5}px;
+    border-radius: 100%;
+    border: ${stroke}px solid rgba(251, 251, 251, 0.05);
+    z-index: -1;
+    box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.25),
+      inset 0px 0px 8px rgba(255, 255, 255, 0.29);
+    backdrop-filter: blur(140px);
+
+    font-style: normal;
+    font-weight: bold;
+    font-size: 48px;
+    line-height: 56px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #0d3740;
+  `
+
+  const Circle = styled.circle`
+              transition: stroke-dashoffset 0.35s;
+            transform: rotate(-90deg);
+            transform-origin: 50% 50%;
+            backdrop-filter: blur(4px);
+  `
 
   return (
-    <div className="progressContainer">
-      <div className="progress">
-        {progress <= 100 ? progress : 100}%
-      </div>
-      <svg
-        height={radius * 2}
-        width={radius * 2}
-      >
+    <Wrapper>
+      <Progress>{progress <= 100 ? progress : 100}%</Progress>
+      <svg height={radius * 2} width={radius * 2}>
         <defs>
-          <linearGradient id='linear' gradientUnits='userSpaceOnUse' x1='-3.52%' y1='4.11%' x2='103.52%' y2='95.89%'>
-            <stop offset='.147' stopColor='#00E0FF' />
-            <stop offset='.673' stopColor='#66D8CA' />
+          <linearGradient
+            id="linear"
+            gradientUnits="userSpaceOnUse"
+            x1="-3.52%"
+            y1="4.11%"
+            x2="103.52%"
+            y2="95.89%"
+          >
+            <stop offset=".147" stopColor="#00E0FF" />
+            <stop offset=".673" stopColor="#66D8CA" />
           </linearGradient>
         </defs>
-        <circle
+        <Circle
           stroke="url(#linear)"
           fill="transparent"
           strokeWidth={stroke}
@@ -29,48 +69,8 @@ export default function Progress({ radius = 100, stroke = 10, progress = 0 }) {
           r={normalizedRadius}
           cx={radius}
           cy={radius}
-        >
-        </circle>
+        ></Circle>
       </svg>
-      <style jsx>
-        {`
-            circle {
-              transition: stroke-dashoffset 0.35s;
-              transform: rotate(-90deg);
-              transform-origin: 50% 50%;
-              backdrop-filter: blur(4px);
-            }
-
-            div.progressContainer {
-              width: 100%;
-              height: 100%;
-              position: relative;
-            }
-
-            div.progress {
-              width: ${(radius * 2) - (stroke * 3)}px;
-              height: ${(radius * 2) - (stroke * 3)}px;
-              position: absolute;
-              top: ${stroke * 1.5}px;
-              left: ${stroke * 1.5}px;
-              border-radius: 100%;
-              border: ${stroke}px solid rgba(251, 251, 251, 0.05);
-              z-index: -1;
-              box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.25), inset 0px 0px 8px rgba(255, 255, 255, 0.29);
-              backdrop-filter: blur(140px);
-
-              font-style: normal;
-              font-weight: bold;
-              font-size: 48px;
-              line-height: 56px;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              text-align: center;
-              color: #0D3740 ;
-            }
-          `}
-      </style>
-    </div>
+    </Wrapper>
   )
 }
